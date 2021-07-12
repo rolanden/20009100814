2cdac9ea4727d2829183d04c58a652b344c2fe89
rolanden <87223050+rolanden@users.noreply.github.com>
2021-07-12 20:45:27 +0800

    Update led.c

--- lesson2/led.c
+++ lesson2/led.c
@@ -1,1 +1,25 @@
+// C++ code
+//
+int i=0;
+void setup()
+{
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+  Serial.begin(9600);
+  Serial.println("I'm OK!");
+}
 
+void loop()
+{
+  for(i=13;i>6;i--)
+  {
+    digitalWrite(i,HIGH);
+    delay(500);
+    digitalWrite(i,LOW);
+  }
+}
