6134574a192da3d51bb9f16c26e29afe42d00069
rolanden <87223050+rolanden@users.noreply.github.com>
2021-07-12 22:00:59 +0800

    Create digitron6.c

---
+++ lesson2/digitron6/digitron6.c
@@ -0,0 +1,23 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(0,HIGH);
+  digitalWrite(1,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
+}
